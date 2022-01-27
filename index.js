5df4d29b90d8ad65ea3f1bb4c38717dbea92afd7
Zac <jiake.chen@jyblife.com>
2022-01-27 16:45:17 +0800

    fix: 暴露部分配置项

--- index.js
+++ index.js
@@ -4,68 +4,97 @@ const woff2 = require('fonteditor-core').woff2;
 const fs = require('fs');
 
 // const baseUnicodePre = 'U+'; // unicode前缀
-const startCHS = '0000'; // 开始字
-const endCHS = '9FCB'; // 结束字
-const pageSize = 5000; // 页长
-let currentCHS = startCHS; // 当前字
-let ISEND = false; // 是否结束切分
-
 /**
- * 累加筛选字
+ * 默认配置
+ * @param startCHS 开始字符unicode编码
+ * @param endCHS 结束字符unicode编码
+ * @param pageSize 每个文件字符包含范围
+ * @param baseFileName 拆分文件基础名
+ * @param targetPath 目标字体包路径
+ * @param targetType 目标字体包类型
  */
-function addSubset(startCHS) {
-  let subset = []; // 筛选列表
-  for (let i = 0; i < pageSize; i++) {
-    const startNum = parseInt(startCHS, 16);
-    if (startNum + i > parseInt(endCHS, 16)) {
-      ISEND = true;
-    } else {
-      subset.push(startNum + i);
-      currentCHS = (startNum + i).toString(16);
-    }
+const defaultConfig = {
+  startCHS: '0000',
+  endCHS: '9FCB',
+  pageSize: 5000,
+  baseFileName: 'HarmonyOS_Sans_SC_Black',
+  targetPath: './font/HarmonyOS_Sans_SC_Black.ttf',
+  targetType: 'ttf',
+};
+
+class FontResolve {
+  constructor(config) {
+    this.config = {
+      ...defaultConfig,
+      ...config,
+    };
+    this.currentCHS = this.config.startCHS;
+    this.resolveComplate = false; // 是否结束切分
   }
-  return {
-    startCHS,
-    endCHS: currentCHS,
-    subset,
-  };
-}
+  /**
+   * 初始化font
+   */
+  initFont() {
+    const { endCHS, pageSize, targetPath, targetType } = this.config;
+    woff2.init().then(() => {
+      const buffer = fs.readFileSync(targetPath);
+      let fontFileIdx = 1;
+      while (!this.resolveComplate) {
+        const subsetMap = this.addSubset(this.currentCHS, endCHS, pageSize);
+        // read font data
+        let font = Font.create(buffer, {
+          type: targetType, // support ttf, woff, woff2, eot, otf, svg
+          subset: subsetMap.subset, // 十进制数组 [65, 66]
+          hinting: true, // save font hinting
+          compound2simple: true, // transform ttf compound glyf to simple
+          inflate: null, // inflate function for woff
+          combinePath: false, // for svg path
+        });
+        // let fontObject = font.get();
 
-function writeFontFile(fontObj, subsetMap, type = 'ttf', fontFileIdx) {
-  // write font file
-  const buffer = fontObj.write({
-    type: type, // support ttf, woff, woff2, eot, svg
-    hinting: true, // save font hinting
-    deflate: null, // deflate function for woff
-    support: { head: {}, hhea: {} }, // for user to overwrite head.xMin, head.xMax, head.yMin, head.yMax, hhea etc.
-  });
-  fs.writeFileSync(
-    `./result/HarmonyOS_Sans_SC_Black_${fontFileIdx}_${subsetMap.startCHS}_${subsetMap.endCHS}.${type}`,
-    buffer,
-  );
-
-  return buffer;
-}
-
-woff2.init().then(() => {
-  let fontFileIdx = 1;
-  while (!ISEND) {
-    const subsetMap = addSubset(currentCHS);
-    let buffer = fs.readFileSync('./font/HarmonyOS_Sans_SC_Black.ttf');
-    // read font data
-    let font = Font.create(buffer, {
-      type: 'ttf', // support ttf, woff, woff2, eot, otf, svg
-      subset: subsetMap.subset || [0, 32, 33, 34, 65, 66], // only read `a`, `b` glyf
-      hinting: true, // save font hinting
-      compound2simple: true, // transform ttf compound glyf to simple
-      inflate: null, // inflate function for woff
-      combinePath: false, // for svg path
+        this.writeFontFile(font, subsetMap, 'ttf', fontFileIdx);
+        this.writeFontFile(font, subsetMap, 'woff', fontFileIdx);
+        this.writeFontFile(font, subsetMap, 'woff2', fontFileIdx);
+        fontFileIdx++;
+      }
     });
-    let fontObject = font.get();
-
-    writeFontFile(font, subsetMap, 'ttf', fontFileIdx);
-    writeFontFile(font, subsetMap, 'woff', fontFileIdx);
-    writeFontFile(font, subsetMap, 'woff2', fontFileIdx);
-    fontFileIdx++;
   }
-});
+  /**
+   * 累加筛选字
+   */
+  addSubset(startCHS, endCHS, pageSize) {
+    let subset = []; // 筛选列表
+    for (let i = 0; i < pageSize; i++) {
+      const startNum = parseInt(startCHS, 16);
+      if (startNum + i > parseInt(endCHS, 16)) {
+        this.resolveComplate = true;
+      } else {
+        subset.push(startNum + i);
+        this.currentCHS = (startNum + i).toString(16);
+      }
+    }
+    return {
+      startCHS,
+      endCHS: this.currentCHS,
+      subset,
+    };
+  }
+  writeFontFile(fontObj, subsetMap, type = 'ttf', fontFileIdx) {
+    // write font file
+    const buffer = fontObj.write({
+      type: type, // support ttf, woff, woff2, eot, svg
+      hinting: true, // save font hinting
+      deflate: null, // deflate function for woff
+      support: { head: {}, hhea: {} }, // for user to overwrite head.xMin, head.xMax, head.yMin, head.yMax, hhea etc.
+    });
+    fs.writeFileSync(
+      `./result/${this.config.baseFileName}_${fontFileIdx}_${subsetMap.startCHS}_${subsetMap.endCHS}.${type}`,
+      buffer,
+    );
+    return buffer;
+  }
+}
+
+const ff = new FontResolve();
+ff.initFont();
+console.log('拆分导出完成！');
